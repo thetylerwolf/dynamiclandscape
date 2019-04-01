98a7ba6b6ccc862c95a5d83227284b8f5365237b
Tyler <tylernicholaswolf@gmail.com>
2019-04-01 21:34:34 +0200

    made a bunch of maps

---
+++ src/components/MapMultiples.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react"
+import goalIdLookup from '../data_set/goalIdMapping.json'
+import SmallMap from './SmallMap'
+
+class MapMultiples extends Component {
+
+  maps() {
+
+    return this.props.data.ids.map(id => {
+      return (
+        <div style={{ width:'300px' }} key={ id }>
+          <div>{ goalIdLookup[ id ] }</div>
+          <SmallMap id={ id } />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    let maps = this.maps()
+
+    return (
+      <div className="multiples-wrap">
+        <div>
+          <h3>{ this.props.data.name }</h3>
+          <div className="maps-wrap" style={{ display:'flex' }}>
+          { maps }
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default MapMultiples;
---
+++ src/components/SmallMap.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react"
+import { feature } from "topojson-client"
+import { ReactComponent as Map } from '../data_set/municipalities.svg'
+import mId from '../data_set/municipalityIdMapping.json'
+import { extent } from 'd3-array'
+import { scaleLinear } from 'd3-scale'
+import { schemeRdBu } from 'd3-scale-chromatic'
+
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = { extent: [] }
+  }
+
+  componentDidMount() {
+    let map = this.refs.swedenMap,
+      id = this.props.id
+
+    fetch(`/goals/${ id }.json`)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log(`Error: ${response.status}`)
+          return
+        }
+        response.json().then(data => {
+
+          data.values.forEach(v => v.value = v.values.find(d => d.gender == 'T'))
+
+          let r = extent(data.values, (d) => d.value.value),
+            scale = scaleLinear()
+              .domain(r)
+              .range([schemeRdBu[3][0], schemeRdBu[3][2]])
+
+
+          data.values.forEach(v => {
+            let id = mId[ v.municipality ]
+            id = id.replace(/ä/g, '\\xE4')
+            id = id.replace(/Ä/g, '\\xC4')
+            id = id.replace(/ö/g, '\\xF6')
+            id = id.replace(/Ö/g, '\\xD6')
+            id = id.replace(/å/g, '\\xE5')
+            id = id.replace(/Å/g, '\\xC5')
+            let path = map.getElementById(id)
+            if(!path) console.log(id, path)
+            if(path)
+              path.style.fill = scale(v.value.value)
+          })
+
+          this.setState({ extent: r })
+        })
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="map-wrap">
+        <Map ref="swedenMap" />
+        <div style={{ color: schemeRdBu[3][2] }}>Max { this.state.extent[1] && this.state.extent[1].toFixed(2) }</div>
+        <div style={{ color: schemeRdBu[3][0] }}>Min { this.state.extent[0] && this.state.extent[0].toFixed(2) }</div>
+      </div>
+    )
+  }
+}
+
+export default App;
