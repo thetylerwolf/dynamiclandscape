3de5c5ac820dd75b63c67c128e5a8d89daef657f
thetylerwolf <tylernicholaswolf@gmail.com>
2019-04-05 22:43:20 +0200

    colros and radius working

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,40 +1,40 @@
 import React, { Component } from "react";
 import { kpiMapping } from "../models/mappings";
-import '../css/SelectionKpis.css'
+import '../css/Dropdown.css'
 import Select from "react-select";
 
 
 
 class Dropdown extends Component {
 
-    state = {
-        selectedOption: null,
-      }
-
-  constructor() {
+  constructor(props) {
     super();
+
+    this.options = [];
+
+    Object.entries(props.data).forEach(([key, value]) => {
+        this.options.push( {value: key, label: value.name} );
+    });
+
+    this.state = {
+      selectedOption: this.options[0],
+    }
   }
 
   onChange(selectedOption){
     this.setState({ selectedOption });
     this.props.onChange({selectedOption})
-    console.log(`Option selected:`, selectedOption);
   }
 
   render() {
     const { selectedOption } = this.state;
-    var options = [];
-
-    Object.entries(this.props.data).forEach(([key, value]) => {
-        options.push( {value: key, label: value.name} );
-    });
 
     return (
         <div className="selection-kpis-dropdown">
         <Select
           value={selectedOption}
           onChange={ (option) => this.onChange(option)}
-          options={options}
+          options={this.options}
           placeholder= {this.props.placeholder}
         />
       </div>
