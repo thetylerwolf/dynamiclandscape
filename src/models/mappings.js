9dc6b7698e9cc37682adbe327bf385cdbf558a93
adamekberg <ekberg.adam@hotmail.com>
2019-04-05 17:00:14 +0200

    import -> require

--- src/models/mappings.js
+++ src/models/mappings.js
@@ -1,16 +1,16 @@
-import goalsData from '../data_set/goals/all_goals_latest_years.json'
-import wasteData from '../data_set/waste/all_waste_latest_years.json'
-import integrationData from '../data_set/integration/all_integration_latest_years.json'
-import energyData from '../data_set/energy/all_energy_latest_years.json'
-import elderlyCareData from '../data_set/elderlyCare/all_elderlyCare_latest_years.json'
-import municipalityIds from '../data_set/municipalityIdMapping.json'
-import wasteIdMapping from '../data_set/mapping/wasteIdMapping.json'
-import integrationIdMapping from '../data_set/mapping/integrationIdMapping.json'
-import goalIdMapping from '../data_set/mapping/goalIdMapping.json'
-import energyIdMapping from '../data_set/mapping/energyIdMapping.json'
-import elderlyCareIdMapping from '../data_set/mapping/elderlyCareIdMapping.json'
+const goalsData = require("../data_set/goals/all_goals_latest_years.json");
+const wasteData = require("../data_set/waste/all_waste_latest_years.json");
+const integrationData = require("../data_set/integration/all_integration_latest_years.json");
+const energyData = require("../data_set/energy/all_energy_latest_years.json");
+const elderlyCareData = require("../data_set/elderlyCare/all_elderlyCare_latest_years.json");
+const municipalityIds = require("../data_set/municipalityIdMapping.json");
+const wasteIdMapping = require("../data_set/mapping/wasteIdMapping.json");
+const integrationIdMapping = require("../data_set/mapping/integrationIdMapping.json");
+const goalIdMapping = require("../data_set/mapping/goalIdMapping.json");
+const energyIdMapping = require("../data_set/mapping/energyIdMapping.json");
+const elderlyCareIdMapping = require("../data_set/mapping/elderlyCareIdMapping.json");
 
-const municipalities = Object.keys(municipalityIds).sort()
+const municipalities = Object.keys(municipalityIds).sort();
 
 const allData = [
   ...goalsData.kpis,
@@ -18,52 +18,40 @@ const allData = [
   ...integrationData.kpis,
   ...energyData.kpis,
   ...elderlyCareData.kpis
-]
+];
 
 const kpiMapping = {
   ...wasteIdMapping,
   ...integrationIdMapping,
   ...goalIdMapping,
   ...energyIdMapping,
-  ...elderlyCareIdMapping,
-}
+  ...elderlyCareIdMapping
+};
 
-console.log('ad', allData)
+console.log("ad", allData);
 
 const nodeData = municipalities.map(muniId => {
-
   return {
     kpis: allData.map(kpi => {
       return {
         ...(kpi.municipalities.find(m => m.id === muniId) || {}),
         id: kpi.id
-      }
+      };
     }),
     id: muniId,
     active: true,
-    name: municipalityIds[ muniId ]
-  }
-
-})
+    name: municipalityIds[muniId]
+  };
+});
 
 let modelData = nodeData.map(muni => {
-
   let kpiArr = muni.kpis.map(kpi => {
+    let v = kpi.value === undefined ? -1 : kpi.value;
 
-    let v = kpi.value === undefined ? -1 : kpi.value
+    return v;
+  });
 
-    return v
-  })
+  return kpiArr;
+});
 
-  return kpiArr
-
-})
-
-
-
-export {
-  municipalityIds,
-  nodeData,
-  modelData,
-  kpiMapping
-}
+export { municipalityIds, nodeData, modelData, kpiMapping };
