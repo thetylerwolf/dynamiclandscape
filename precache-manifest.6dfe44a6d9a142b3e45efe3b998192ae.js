031825e9e2b4795d3bf85a5b3d37c02d1a6be0e5
thetylerwolf <tylernicholaswolf@gmail.com>
2019-04-06 08:53:56 +0200

    Updates

---
+++ precache-manifest.6dfe44a6d9a142b3e45efe3b998192ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79a091d6492a89119d756a693fdda59b",
+    "url": "/static/media/municipalities.79a091d6.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "95fcae21dee5ae6e7bb7",
+    "url": "/static/js/main.ed13c234.chunk.js"
+  },
+  {
+    "revision": "7596b97450d99122b535",
+    "url": "/static/js/2.fbcad81e.chunk.js"
+  },
+  {
+    "revision": "95fcae21dee5ae6e7bb7",
+    "url": "/static/css/main.999b5277.chunk.css"
+  },
+  {
+    "revision": "a6439f4bbf7558d787f54af89b435b66",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hackforsweden/precache-manifest.21031ca423455559b47c4826d4ce3767.js"
+  "/precache-manifest.6dfe44a6d9a142b3e45efe3b998192ae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hackforsweden/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
